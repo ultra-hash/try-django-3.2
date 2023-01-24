7900ad16e1fbbe7d875f72b2725888d09096eb6e
ultra-hash <jarviscommand3@gmail.com>
2023-01-24 07:26:20 +0530

    added some data in home_view

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,14 +2,17 @@
 To render html web pages
 """
 from django.http import HttpResponse
+import random 
 
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
 
 def home_view(request):
     """
     Take in a request (Django sends request)
     Return HTML as a responce (we pick to return the response)
+    """
+    name = "Swamy"
+    number = random.randint(10, 12321)
+    HTML_STRING = f"""
+    <h1>Hello {name} - {number}</h1>
     """
     return HttpResponse(HTML_STRING)
